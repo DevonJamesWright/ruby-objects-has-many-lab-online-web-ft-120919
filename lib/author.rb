84f94a71fbad1c2a309a2f66a73f8b5bd715f338
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-06 19:56:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,23 @@
 class Author 
-  attr_accessor :name
-  def initialize (name)
+  attr_accessor :name, :genre 
+  def initialize(name)
     @name = name
-    
   end
-end 
+  
+  def posts 
+    Post.all.select {|postie| postie.author == self}
+  end
+  
+  def add_post(postie)
+    postie.author = self
+  end
+  
+  def add_post_by_name(name)
+    postie = Post.new(name)
+    add_post(postie)
+  end 
+  def post_count
+    self.length
+    
+  end 
+end
