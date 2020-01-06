9675744be432580436ca6831112542a96d224f03
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-06 19:46:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,16 @@
 class Song 
-attr_accessor :artist, :genre, :name
-@@all = []
-def initialize (name)
-  @name = name 
-  save
-end 
+  attr_accessor :artist, :genre, :name
+    @@all = []
+  def initialize (name)
+    @name = name 
+    save
+  end 
 
-def save(song)
-  @@all << song
-end
+  def save(song)
+    @@all << self
+  end
+
+  def self.all
+   @@all 
+  end 
 end
